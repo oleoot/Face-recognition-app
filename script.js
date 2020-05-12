0e17610dd2d88750e470bbb27e55ae5c7707641c
oleoot <oleoot28@gmail.com>
2020-05-12 17:16:46 +0300

    Canvas adjustment

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
     document.body.append(canvas);
     const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
